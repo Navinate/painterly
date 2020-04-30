7ddaef6574f236383027fd2ab7f6d10f9075750e
Navinate <treycluff@gmail.com>
2020-04-30 17:12:38 -0400

    making the random a bit less random

--- js/entry.js
+++ js/entry.js
@@ -18,11 +18,11 @@
 
 
     function getQueryVariable(variable) { 
-      var query = window.location.search.substring(1); 
-      var vars = query.split("&"); 
-      for (var i=0;i<vars.length;i++)
+      let query = window.location.search.substring(1); 
+      let vars = query.split("&"); 
+      for (let i=0;i<vars.length;i++)
       { 
-        var pair = vars[i].split("="); 
+        let pair = vars[i].split("="); 
         if (pair[0] == variable)
         { 
           console.log("Loading data: "+pair[1]);
@@ -47,7 +47,9 @@
       })
     }
 
-
+    function findID(arg) {
+      return document.getElementById(arg);
+  }
    
 
 })();
--- js/helper.js
+++ js/helper.js
@@ -13,9 +13,13 @@
     }
 
     function randomID() {
-        let rand = Math.random()*5 + 1;
+        let rand = Math.floor(Math.random()*5 + 1);
+        console.log("substring: "+window.location.href.substring(32))
+        if(window.location.href.substring(1) == window.location.href.substring(1)) {
         findID("random_link").href = "entry.html?id="+rand;
-
+        } else {
+            randomID();
+        }
     }
 
     function findID(arg) {
