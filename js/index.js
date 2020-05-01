13a1371c228d0d9efd5c63c3757d1679f6c38661
Navinate <treycluff@gmail.com>
2020-05-01 14:24:54 -0400

    was adding alt to link, not img

--- js/index.js
+++ js/index.js
@@ -15,21 +15,25 @@
         for (let i = 1; i <= num; i++) {
             let resp = document.createElement("div");
             resp.classList.add("responsive");
+
             let gallery = document.createElement("div");
             gallery.classList.add("gallery");
+
             let link = document.createElement("a");
             link.href = "entry.html?id="+i;
+
             let image = document.createElement("img");
             image.src = "art/paint"+i+".png";
+
             let info = document.createElement("div");
             info.classList.add("info");
+
             //retrieving data from the json object
             $.get("../art/" + i + ".json", function(data, status) {
                 console.log("Retrivial status: " + status);
                 info.innerHTML = "<h2>"+data.title+"</h2><p>By: "+data.artist+"</p>";
                 //adds alt tags to generated images
-                link.alt = ""+ data.description;
-                console.log(data);
+                image.alt = data.description;
             })
             link.appendChild(image);
             gallery.appendChild(link);
