e5ebc55abaca0c555b6ed3d9726d34f345a3f260
Navinate <treycluff@gmail.com>
2020-05-01 14:13:56 -0400

    added debuig statement for why stupid json

--- js/index.js
+++ js/index.js
@@ -21,13 +21,15 @@
             link.href = "entry.html?id="+i;
             let image = document.createElement("img");
             image.src = "art/paint"+i+".png";
-            
             let info = document.createElement("div");
             info.classList.add("info");
+            //retrieving data from the json object
             $.get("../art/" + i + ".json", function(data, status) {
                 console.log("Retrivial status: " + status);
                 info.innerHTML = "<h2>"+data.title+"</h2><p>By: "+data.artist+"</p>";
+                //adds alt tags to generated images
                 link.alt = data.description;
+                console.log(data);
             })
             link.appendChild(image);
             gallery.appendChild(link);
